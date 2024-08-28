e2ef56acdea1ae68c40a04bb32641822033b01a0
AriePriyambadha <6871368+AriePriyambadha@users.noreply.github.com>
2024-08-28 15:25:53 +0700

    Create html, css, and js for barbone files

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+const result = document.querySelector("#result");
+const num1 = document.querySelector("#btn-num1");
+const num2 = document.querySelector("#btn-num2");
+const num3 = document.querySelector("#btn-num3");
+const num4 = document.querySelector("#btn-num4");
+const num5 = document.querySelector("#btn-num5");
+const num6 = document.querySelector("#btn-num6");
+const num7 = document.querySelector("#btn-num7");
+const num8 = document.querySelector("#btn-num8");
+const num9 = document.querySelector("#btn-num9");
+const num0 = document.querySelector("#btn-num0");
+const add = document.querySelector("#btn-add");
+let currNum;
+
+/*
+  EVENT HANDLING WHEN BUTTON CLICK !!
+*/
+num1.addEventListener("click", () => {
+  if (result.textContent === "0") {
+    result.textContent = "1";
+    currNum = 1;
+  } else {
+    result.textContent += "1";
+    currNum += 1;
+  }
+});
+
+num2.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "2";
+  else result.textContent += "2";
+});
+
+num3.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "3";
+  else result.textContent += "3";
+});
+
+num4.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "4";
+  else result.textContent += "4";
+});
+
+num5.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "5";
+  else result.textContent += "5";
+});
+
+num6.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "6";
+  else result.textContent += "6";
+});
+
+num7.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "7";
+  else result.textContent += "7";
+});
+
+num8.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "8";
+  else result.textContent += "8";
+});
+
+num9.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "9";
+  else result.textContent += "9";
+});
+
+num0.addEventListener("click", () => {
+  if (result.textContent === "0") result.textContent = "0";
+  else result.textContent += "0";
+});
+
+add.addEventListener("click", () => {});
